17e7fd06fe38080250b5e92ffdb40b8dab2f7d71
michaelasocolof <michaelasocolof@vpn72228.vpn.mcgill.ca>
2016-06-17 15:50:59 -0400

    gp czech

--- SCT/utteranceencodingbenchmark.py
+++ SCT/utteranceencodingbenchmark.py
@@ -51,10 +51,10 @@ def utterance_encoding_run_query(data):
     sd = statistics.stdev(times)
     return [(end - beg), avgtime, sd]
 
-buckeye_utts = utterance_encoding_run_query(buckeyebenchmark)
+#buckeye_utts = utterance_encoding_run_query(buckeyebenchmark)
 globalphone_utts = utterance_encoding_run_query(globalphonebenchmark)
 #sotc_utts = utterance_encoding_run_query(sotcbenchmark)
-timit_utts = utterance_encoding_run_query(timitbenchmark)
+#timit_utts = utterance_encoding_run_query(timitbenchmark)
 
 def WriteDictToCSV(csv_file,csv_columns,dict_data):
         with open(csv_file, 'w') as csvfile:
@@ -66,10 +66,10 @@ def WriteDictToCSV(csv_file,csv_columns,dict_data):
 
 csv_columns = ['Computer','Date','Corpus', 'Type of benchmark', 'Total time', 'Mean time per call back', 'sd time between call backs']
 dict_data = [
-    {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': amountofcorpus + 'buckeye', 'Type of benchmark': 'Utterance encoding', 'Total time': buckeye_utts[0], 'Mean time per call back': buckeye_utts[1], 'sd time between call backs': buckeye_utts[2]},
+    #{'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': amountofcorpus + 'buckeye', 'Type of benchmark': 'Utterance encoding', 'Total time': buckeye_utts[0], 'Mean time per call back': buckeye_utts[1], 'sd time between call backs': buckeye_utts[2]},
     {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': amountofcorpus + 'globalphone', 'Type of benchmark': 'Utterance encoding', 'Total time': globalphone_utts[0], 'Mean time per call back': globalphone_utts[1], 'sd time between call backs': globalphone_utts[2]},
     #{'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': amountofcorpus + 'sotc', 'Type of benchmark': 'Utterance encoding', 'Total time': sotc_utts[0], 'Mean time per call back': sotc_utts[1], 'sd time between call backs': sotc_utts[2]},
-    {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': amountofcorpus + 'timit', 'Type of benchmark': 'Utterance encoding', 'Total time': timit_utts[0], 'Mean time per call back': timit_utts[1], 'sd time between call backs': timit_utts[2]},
+    #{'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': amountofcorpus + 'timit', 'Type of benchmark': 'Utterance encoding', 'Total time': timit_utts[0], 'Mean time per call back': timit_utts[1], 'sd time between call backs': timit_utts[2]},
     ]
 
 currentPath = os.getcwd()
@@ -88,8 +88,8 @@ csv_file = 'benchmark'+date+'.csv'
 with open('benchmark'+date+'.csv', 'a') as csv_file:
 	writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
 	writer.writerow(dict_data[0])
-	writer.writerow(dict_data[1])
-	writer.writerow(dict_data[2])
+	#writer.writerow(dict_data[1])
+	#writer.writerow(dict_data[2])
 	#writer.writerow(dict_data[3])
 
 
