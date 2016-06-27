c8215f948f10ab81853fed998e68b3a372acb2fe
michaelasocolof <michaelasocolof@wpa073229.wireless.mcgill.ca>
2016-06-27 10:22:07 -0400

    czech encoding

--- SCT/globalphone_pss.py
+++ SCT/globalphone_pss.py
@@ -88,7 +88,35 @@ def speech_rate_phones(data):
 def speech_rate_syllables(data):
    beg = time.time()
    with CorpusContext(data, **graph_db) as c:
-       c.encode_rate('utterance', 'syllable', 'speech_rate_syllable')
+       c.encode_rate('utterance', 'syllable', 'speech_rate_syllables')
+   end = time.time()
+   return [(end-beg), None]
+
+def number_of_syllables(data):
+    beg = time.time()
+   with CorpusContext(data, **graph_db) as c:
+       c.encode_rate('word', 'syllable', 'number_of_syllables')
+   end = time.time()
+   return [(end-beg), None]
+
+def number_of_phones(data):
+    beg = time.time()
+   with CorpusContext(data, **graph_db) as c:
+       c.encode_rate('syllable', 'phone', 'number_of_phones')
+   end = time.time()
+   return [(end-beg), None]
+
+def position_in_utterance(data):
+    beg = time.time()
+   with CorpusContext(data, **graph_db) as c:
+       c.encode_rate('utterance', 'word', 'number_of_phones')
+   end = time.time()
+   return [(end-beg), None]
+
+def position_in_word(data):
+    beg = time.time()
+   with CorpusContext(data, **graph_db) as c:
+       c.encode_rate('word', 'syllable', 'number_of_phones')
    end = time.time()
    return [(end-beg), None]
 
@@ -103,7 +131,7 @@ def export_query_pss(data, export_path):
         columns = (c.syllable.word.label, c.syllable.word.duration, c.syllable.word.begin, c.syllable.word.end,
             c.syllable.word.number_of_syllables, c.syllable.word.position_in_utterance,
             c.pause.following.duration, c.pause.following.label,
-            c.syllable.utterance.speech_rate_phones, c.syllable.utterance.speech_rate_syllables, c.syllable.utterance.begin,
+            c.syllable.utterance.speech_rate_phones, c.syllable.utterance.speech_rate_syllable, c.syllable.utterance.begin,
             c.syllable.utterance.end, c.syllable.utterance.number_of_words,
             c.syllable.discourse, c.syllable.speaker,
             c.syllable.duration, c.syllable.label, c.syllable.position_in_word, c.syllable.number_of_phones)
@@ -119,7 +147,11 @@ def export_query_pss(data, export_path):
 #globalphone_syllables = syllable_encoding_run_query(globalphonebenchmark)
 #globalphone_speechrate_phones = speech_rate_phones(globalphonebenchmark)
 #globalphone_speechrate_syllables = speech_rate_syllables(globalphonebenchmark)
-globalphone_export_pss = export_query_pss(globalphonebenchmark, 'exportbenchmark.csv')
+globalphone_num_syllables = number_of_syllables(globalphonebenchmark)
+globalphone_num_phones = number_of_phones(globalphonebenchmark)
+globalphone_word_position = position_in_utterance(globalphonebenchmark)
+globalphone_syllable_position = position_in_word(globalphonebenchmark)
+#globalphone_export_pss = export_query_pss(globalphonebenchmark, 'exportbenchmark.csv')
 
 def WriteDictToCSV(csv_file,csv_columns,dict_data):
         with open(csv_file, 'w') as csvfile:
@@ -137,7 +169,11 @@ dict_data = [
     #{'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Syllable encoding', 'Total time': globalphone_syllables[0], 'Mean time per call back': None, 'sd time between call backs': None},
     #{'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Speech rate encoding (phones)', 'Total time': globalphone_speechrate_phones[0], 'Mean time per call back': None, 'sd time between call backs': None},
     #{'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Speech rate encoding (syllables)', 'Total time': globalphone_speechrate_syllables[0], 'Mean time per call back': None, 'sd time between call backs': None},
-    {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Export polysyllabic shortening', 'Total time': globalphone_export_pss[0], 'Mean time per call back': None, 'sd time between call backs': None},
+    {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Num syllables encoding', 'Total time': globalphone_num_syllables[0], 'Mean time per call back': None, 'sd time between call backs': None},
+    {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Num phones encoding', 'Total time': globalphone_num_phones[0], 'Mean time per call back': None, 'sd time between call backs': None},
+    {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Word position encoding', 'Total time': globalphone_word_position[0], 'Mean time per call back': None, 'sd time between call backs': None},
+    {'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Syllable position encoding', 'Total time': globalphone_syllable_position[0], 'Mean time per call back': None, 'sd time between call backs': None},
+    #{'Computer': platform.node(), 'Date': str(datetime.now()), 'Corpus': globalphonebenchmark, 'Type of benchmark': 'Export polysyllabic shortening', 'Total time': globalphone_export_pss[0], 'Mean time per call back': None, 'sd time between call backs': None},
     ]
 
 now = datetime.now()
@@ -154,9 +190,9 @@ csv_file = 'benchmark'+date+'.csv'
 with open('benchmark'+date+'.csv', 'a') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
     writer.writerow(dict_data[0])
-    #writer.writerow(dict_data[1])
-    #writer.writerow(dict_data[2])
-    #writer.writerow(dict_data[3])
+    writer.writerow(dict_data[1])
+    writer.writerow(dict_data[2])
+    writer.writerow(dict_data[3])
     #writer.writerow(dict_data[4])
     #writer.writerow(dict_data[5])
 
