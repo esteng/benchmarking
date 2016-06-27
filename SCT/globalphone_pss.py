d07212bd97d950b68666515a873c8e95264cbfd2
michaelasocolof <michaelasocolof@wpa073229.wireless.mcgill.ca>
2016-06-27 12:01:25 -0400

    czech enrichment

--- SCT/globalphone_pss.py
+++ SCT/globalphone_pss.py
@@ -22,8 +22,8 @@ amountofcorpus = 'full'
 #globalphonebenchmark = 'globalphonebenchmark'
 globalphonebenchmark = 'globalphone_cz'
 
-#globalphonesyllabic = ['a', 'aa', 'aw', 'e', 'ee', 'ew', 'i', 'ii', 'o', 'oo', 'ow', 'u', 'uu']#cz
-globalphonesyllabic = ['ab', 'e', 'i', 'i2', 'o', 'oe', 'u', 'ue']#tu
+globalphonesyllabic = ['a', 'aa', 'aw', 'e', 'ee', 'ew', 'i', 'ii', 'o', 'oo', 'ow', 'u', 'uu']#cz
+#globalphonesyllabic = ['ab', 'e', 'i', 'i2', 'o', 'oe', 'u', 'ue']#tu
 
 lasttime = time.time()
 times = []
