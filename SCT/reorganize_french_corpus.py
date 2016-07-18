a90f4b56acc309f1682bcd9c2b23a74ddd494e11
michaelasocolof <michaelasocolof@vpn73229.vpn.mcgill.ca>
2016-07-18 13:29:19 -0400

    french sample rate

--- SCT/reorganize_french_corpus.py
+++ SCT/reorganize_french_corpus.py
@@ -138,7 +138,8 @@ for i in subjectids.keys():
 
 for root, dirs, files in os.walk(corpus):
     for f in files:
-        subprocess.call(['sox', f.replace('\\','/'), f.replace('\\','/'),
+    	filepath = os.path.join(root, f)
+        subprocess.call(['sox', filepath.replace('\\','/'), filepath.replace('\\','/'),
                         'gain', '-1', 'rate', '-I', str(new_sr)])
 
 
