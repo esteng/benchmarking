ab82385779e289564d17d61d22e8b04fdbb1b7fc
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-07-20 21:38:45 -0400

    fix bug

--- reorganize_french_corpus.py
+++ reorganize_french_corpus.py
@@ -167,18 +167,18 @@ for root, dirs, files in os.walk(corpus):
                         'gain', '-1', 'rate', '-I', str(new_sr)])'''
 
 
-original_path = '/media/share/datasets/aligner_benchmarks/AlignerTestData/2_French_2000files'
-new_path = '/media/share/datasets/aligner_benchmarks/sorted_quebec_french'
+old_directory = '/media/share/datasets/aligner_benchmarks/AlignerTestData/2_French_2000files'
+new_directory = '/media/share/datasets/aligner_benchmarks/sorted_quebec_french'
 
-for f in os.listdir(original_path):
+for f in os.listdir(old_directory):
     if not any(f.lower().endswith(x) for x in ['.wav', '.lab']):
         continue
     name, ext = os.path.splitext(f)
     splitname = name.split('_')
     speaker_id = '_'.join(splitname[:2])
-    speaker_dir = os.path.join(new_path, speaker_id)
+    speaker_dir = os.path.join(new_directory, speaker_id)
     os.makedirs(speaker_dir, exist_ok = True)
 
-    old_path = os.path.join(original_path, f)
+    old_path = os.path.join(old_directory, f)
     new_path = os.path.join(speaker_dir, f)
     shutil.copyfile(old_path, new_path)
