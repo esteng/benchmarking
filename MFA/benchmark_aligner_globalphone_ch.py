f500f583788bf5a64e0bf2ab41207423c81bd03d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-01-18 14:23:34 -0500

    Remove clean

--- MFA/benchmark_aligner_globalphone_ch.py
+++ MFA/benchmark_aligner_globalphone_ch.py
@@ -23,7 +23,7 @@ class DummyArgs(object):
         self.fast = False
         self.speaker_characters = 0
         self.verbose = False
-        self.clean = True
+        self.clean = False
         self.no_speaker_adaptation = False
 
 args = DummyArgs()
